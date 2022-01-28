19b0eb79efe195aba84cc1f9dbceafbd0c8f3dc2
Heidi Hlavinka <hchlav@gmail.com>
2022-01-28 14:14:21 -0600

    commit tree f1f25d5079b1ed7b239dfc0886f108fd431fffbd

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarves)
+  dwarves.collect.with_index do |word, i|
+    puts "#{i+1}. #{word}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect do |word|
+    "#{word.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+    calls_long.any? do |word|
+      word.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+    cheese_types.each do |word|
+      if array.include?(word)
+        return word
+      else
+        return nil
+      end
+    end
 end
+# binding.pry
